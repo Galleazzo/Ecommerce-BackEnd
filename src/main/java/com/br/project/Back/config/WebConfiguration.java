e5504acd9439daedf0a326a30eaae10cfa84d7d4
Galleazzo <paulogallezzo1@gmail.com>
2023-11-05 16:00:31 -0300

    firebase auth

--- src/main/java/com/br/project/Back/config/WebConfiguration.java
+++ src/main/java/com/br/project/Back/config/WebConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,31 +16,15 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfigurations {
+public class WebConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private SecurityFilter securityFilter;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity.csrf().disable()
-                        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                        .and().authorizeRequests()
-                        .antMatchers(HttpMethod.POST, "/login").permitAll()
-                        .anyRequest().authenticated()
-                        .and().addFilterBefore(this.securityFilter, UsernamePasswordAuthenticationFilter.class)
-                        .build();
+        http.oauth2ResourceServer()
+                .jwt();
     }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
-        return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
 }
