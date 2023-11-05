2aae4f73bfbc963f3ace2000e22c8f0c829b890e
Paulo Galleazzo <paulo.galeazzo@cittamobi.com.br>
2023-11-05 16:00:31 -0300

    firebase auth

--- src/main/java/com/br/project/Back/controller/AutenticationController.java
+++ src/main/java/com/br/project/Back/controller/AutenticationController.java
@@ -1,9 +1,6 @@
 package com.br.project.Back.controller;
 
 import com.br.project.Back.model.User;
-import com.br.project.Back.model.dto.UserDTO;
-import com.br.project.Back.model.dto.tokenDTO;
-import com.br.project.Back.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,19 +15,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class AutenticationController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private TokenService tokenService;
-
-    @PostMapping
-    public ResponseEntity login(@RequestBody User user){
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
-        Authentication authentication =  this.authenticationManager.authenticate(authenticationToken);
-
-        String tokenJWT = this.tokenService.generateToken((User) authentication.getPrincipal());
-
-        return ResponseEntity.ok(new tokenDTO(tokenJWT));
-    }
 }
